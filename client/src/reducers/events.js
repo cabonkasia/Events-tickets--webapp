c1522621873b50546f2adc99286f3cd87f432ab8
cabonkasia <cabonkc@gmail.com>
2019-02-12 15:20:14 +0100

    bug fix in events reducer

--- client/src/reducers/events.js
+++ client/src/reducers/events.js
@@ -3,7 +3,7 @@ import { EVENTS_FETCHED } from '../actions/events'
 export default (state = null, action) => {
   switch (action.type) {
     case EVENTS_FETCHED:
-    return action.events
+    return action.events.events
     
     default:
     return state
