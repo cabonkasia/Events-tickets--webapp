7b0eb83a29ebb6c700444d9677ce07dcebef6299
cabonkasia <cabonkc@gmail.com>
2019-02-12 16:21:13 +0100

    return event name in TicketsList if no tickets

--- client/src/components/tickets/TicketsList.js
+++ client/src/components/tickets/TicketsList.js
@@ -2,16 +2,22 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export default (props) => {
-    if(!props.event || props.event.tickets.length === 0) 
-    return <h1>No tickets.</h1>
+    if(!props.event)
+    return <h3>Loading tickets...</h3>
 
+    if(props.event.tickets.length === 0) 
+    return (
+        <main>
+        <h1>{props.event.name}</h1>
+        <h3>No tickets.</h3>
+        </main>
+        )
 
     return (
       <div>
         <h3>TICKETS AVAILABLE FOR:  {props.event.name}</h3>
         <ol>
             { props.event.tickets.map(ticket => {
-                console.log(props.event.tickets)
                 return <Link to={`/events/${props.event.id}/tickets/${ticket.id}`}>
                 <li>
                 <img src={ticket.picture} alt={ticket.imgalt}/><br/>
